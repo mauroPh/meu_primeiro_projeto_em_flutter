a7f98b9afc39324d47138a170e18e86f9995c9f2
Mauro Philipe <63133867+mauroPh@users.noreply.github.com>
2023-01-04 14:04:07 -0300

    encerrando primeira tela em dart com flutter

--- lib/main.dart
+++ lib/main.dart
@@ -11,114 +11,15 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Flutter: estudando o Scaffold!'),
         ),
-        home: Container(
-          color: Colors.white,
-          child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Stack(
-                  alignment: AlignmentDirectional.center,
-                  children: [
-                    Container(
-                      color: Colors.blue,
-                      width: 100,
-                      height: 100,
-                    ),
-                    Container(
-                      color: Colors.red,
-                      width: 50,
-                      height: 50,
-                    ),
-                  ],
-                ),
-                Stack(
-                  alignment: AlignmentDirectional.center,
-                  children: [
-                    Container(
-                      color: Colors.red,
-                      width: 100,
-                      height: 100,
-                    ),
-                    Container(
-                      color: Colors.blue,
-                      width: 50,
-                      height: 50,
-                    ),
-                  ],
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Container(
-                      color: Colors.cyan,
-                      width: 50,
-                      height: 50,
-                    ),
-                    Container(
-                      color: Colors.pinkAccent,
-                      width: 50,
-                      height: 50,
-                    ),
-                    Container(
-                      color: Colors.purple,
-                      width: 50,
-                      height: 50,
-                    ),
-                  ],
-                ),
-                Container(
-                  color: Colors.amber,
-                  height: 30,
-                  width: 30000,
-                  child: Text(
-                    'Lorem Ipsum',
-                    style: TextStyle(color: Colors.black, fontSize: 26),
-                    textAlign: TextAlign.center,
-                  ),
-                ),
-                ElevatedButton(
-                    onPressed: () {
-                      print('Você clicou no botão!');
-                    },
-                    child: Text('Clique no botão!'))
-              ]),
-        ));
-
-    /*Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Container(color: Colors.blue,width: 100,height: 100,),
-          Container(color: Colors.red,width: 50,height: 50,),
-        ],
-      )*/
-
-    /*Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: [
-
-            Container(color: Colors.blue,width: 100,height: 100,),
-            Container(color: Colors.red,width: 50,height: 50,),
-          ],
-      )*/
-
-    /*Stack(
-        alignment: AlignmentDirectional.center,
-        children: [
-          Container(color: Colors.blue,width: 100,height: 100,),
-          Container(color: Colors.red,width: 50,height: 50,),
-        ],
-      ),*/
-    /* Container não consegue se alinhar na tela ou colocar outro Container na frente dele,
-      mas com o Stack(), ou com alguns outros Widgets, temos essa possibilidade.
-      Container ao lado do outro. Não conseguimos fazer isso com o Stack()
-    );*/
+      ),
+    );
   }
 }
---
+++ lib/primeira_tela.dart
@@ -0,0 +1,106 @@
+
+/*Container(
+          color: Colors.white,
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Stack(
+                  alignment: AlignmentDirectional.center,
+                  children: [
+                    Container(
+                      color: Colors.blue,
+                      width: 100,
+                      height: 100,
+                    ),
+                    Container(
+                      color: Colors.red,
+                      width: 50,
+                      height: 50,
+                    ),
+                  ],
+                ),
+                Stack(
+                  alignment: AlignmentDirectional.center,
+                  children: [
+                    Container(
+                      color: Colors.red,
+                      width: 100,
+                      height: 100,
+                    ),
+                    Container(
+                      color: Colors.blue,
+                      width: 50,
+                      height: 50,
+                    ),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Container(
+                      color: Colors.cyan,
+                      width: 50,
+                      height: 50,
+                    ),
+                    Container(
+                      color: Colors.pinkAccent,
+                      width: 50,
+                      height: 50,
+                    ),
+                    Container(
+                      color: Colors.purple,
+                      width: 50,
+                      height: 50,
+                    ),
+                  ],
+                ),
+                Container(
+                  color: Colors.amber,
+                  height: 30,
+                  width: 30000,
+                  child: Text(
+                    'Lorem Ipsum',
+                    style: TextStyle(color: Colors.black, fontSize: 26),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                ElevatedButton(
+                    onPressed: () {
+                      print('Você clicou no botão!');
+                    },
+                    child: Text('Clique no botão!'))
+              ]),
+        ));*/
+
+    /*Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(color: Colors.blue,width: 100,height: 100,),
+          Container(color: Colors.red,width: 50,height: 50,),
+        ],
+      )*/
+
+    /*Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+
+            Container(color: Colors.blue,width: 100,height: 100,),
+            Container(color: Colors.red,width: 50,height: 50,),
+          ],
+      )*/
+
+    /*Stack(
+        alignment: AlignmentDirectional.center,
+        children: [
+          Container(color: Colors.blue,width: 100,height: 100,),
+          Container(color: Colors.red,width: 50,height: 50,),
+        ],
+      ),*/
+    /* Container não consegue se alinhar na tela ou colocar outro Container na frente dele,
+      mas com o Stack(), ou com alguns outros Widgets, temos essa possibilidade.
+      Container ao lado do outro. Não conseguimos fazer isso com o Stack()
+    );*/
