33a34c31bea2c404ae450aa31dfb6c05f4661769
mauroPh <mauro.philipe.santos@gmail.com>
2023-01-06 17:15:56 -0300

    add difficult icons(stars)

--- lib/main.dart
+++ lib/main.dart
@@ -105,13 +105,49 @@ class _TaskState extends State<Task> {
                           fit: BoxFit.cover,
                         ),
                       ),
-                      Container(
-                        width: 200,
-                        child: Text(
-                          widget.name,
-                          style: TextStyle(
-                              fontSize: 16, overflow: TextOverflow.ellipsis),
-                        ),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Container(
+                            width: 350,
+                            child: Text(
+                              widget.name,
+                              style: TextStyle(
+                                  fontSize: 22,
+                                  overflow: TextOverflow.ellipsis),
+                            ),
+                          ),
+                          Row(
+                            children: [
+                              Icon(
+                                Icons.star,
+                                size: 15,
+                                color: Colors.blue,
+                              ),
+                              Icon(
+                                Icons.star,
+                                size: 15,
+                                color: Colors.blue,
+                              ),
+                              Icon(
+                                Icons.star,
+                                size: 15,
+                                color: Colors.blue,
+                              ),
+                              Icon(
+                                Icons.star,
+                                size: 15,
+                                color: Colors.lightBlue[100],
+                              ),
+                              Icon(
+                                Icons.star,
+                                size: 15,
+                                color: Colors.lightBlue[100],
+                              ),
+                            ],
+                          )
+                        ],
                       ),
                       Container(
                         height: 52,
